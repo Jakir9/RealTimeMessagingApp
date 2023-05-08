e8e411658e6a0855102c16207505d263b1527201
Jakir9 <jashraf98@hotmail.co.uk>
2023-05-08 20:58:33 +0100

    working server

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-var express = require("express"); //import expressJS
-var app = express(); //create express object
-var port = 1010; //port number
+// Import necessary modules
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
-app.get("/", function (req, res) {
-  //get request
-  res.send("Hello World");
+// Set up the server
+var port = 1010;
+http.listen(port, function () {
+  console.log(`Server running at http://localhost:${port}`);
 });
 
-app.listen(port, function () {
-  //listen to port
-  console.log(`Server running at port: http://127.0.0.1:${port}`);
-}); //end of listen
+app.use(express.static(__dirname + "/public"));
+
+// Set up socket.io
+io.sockets.on("connection", function (socket) {
+  socket.emit("message", { message: "welcome to the chat" });
+  socket.on("send", function (data) {
+    io.sockets.emit("message", data);
+  });
+});
+
+// Set up views
+app.set("views", __dirname + "/tpl");
+app.set("view engine", "jade");
+app.engine("jade", require("jade").__express);
+app.get("/", function (req, res) {
+  res.render("page");
+});
